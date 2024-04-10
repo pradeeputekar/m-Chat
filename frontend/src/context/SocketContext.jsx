485228c8eca6702ea5d7af43583e3325d414850e
pradeeputekar <pradeep.utekar.6@gmail.com>
2024-04-10 21:41:28 +0530

    socket url updated for production

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("http://localhost:5000", {
+      const socket = io("https://m-chat-d6vw.onrender.com", {
         query: {
           userId: authUser._id,
         },
